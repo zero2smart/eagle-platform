2d7bedf36c62cd6e3df955fab82518589ef77a2f
Scott Shervington <scott@bigbadmachine.com>
2019-02-07 13:28:54 -0500

    Moved react files to /client

--- client/packs/dispatch-bundle.js
+++ client/packs/dispatch-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import DispatchApp from '../bundles/Dispatch/startup/DispatchApp';
+import DispatchApp from '../app/bundles/Dispatch/startup/DispatchApp';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
