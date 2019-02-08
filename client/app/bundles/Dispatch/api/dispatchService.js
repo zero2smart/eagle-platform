d07e5ca543608e9ad2014d19ab82be571f76c88f
Scott Shervington <scott@bigbadmachine.com>
2019-02-08 01:31:09 -0500

    Updated dispatchService.js

--- client/app/bundles/Dispatch/api/dispatchService.js
+++ client/app/bundles/Dispatch/api/dispatchService.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
+const API_VERSION = process.env.REACT_APP_API_VERSION;
+
 export default {
   fetchJobs: () => {
-    return axios.get(process.env.REACT_APP_API_DOMAIN + process.env.REACT_APP_API_VERSION + '/dispatch.json', { crossdomain: true });
+    console.log("REACT_APP_API_DOMAIN: " + API_DOMAIN)
+    console.log("REACT_APP_API_VERSION: " + API_VERSION)
+    console.log(API_DOMAIN + API_VERSION + '/dispatch.json')
+    return axios.get(API_DOMAIN + API_VERSION + '/dispatch.json', { crossdomain: true });
   },
   fetchAvailableTrucks: () => {
-    return axios.get(process.env.REACT_APP_API_DOMAIN + process.env.REACT_APP_API_VERSION + '/trucks.json', { crossdomain: true });
+    return axios.get(API_DOMAIN + API_VERSION + '/trucks.json', { crossdomain: true });
   }
 }
