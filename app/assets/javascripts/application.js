262dc4c3203a06d77f1d5e6e9e23f4714a1867ef
Scott Shervington <scott@bigbadmachine.com>
2019-02-08 12:27:30 -0500

    Removed emoty jsasset folders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree ./pages
 //= require theme/base
--- app/assets/javascripts/dispatch.js
+++ app/assets/javascripts/dispatch.js
@@ -15,5 +15,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
-//= require_tree ./dispatch
