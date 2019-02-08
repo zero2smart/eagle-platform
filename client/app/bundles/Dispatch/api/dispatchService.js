5f047b35c980ec1150683e5d67c51ff3e690bbb4
Scott Shervington <scott@bigbadmachine.com>
2019-02-08 00:30:29 -0500

    Adding debug info to env variables

--- client/app/bundles/Dispatch/api/dispatchService.js
+++ client/app/bundles/Dispatch/api/dispatchService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export default {
   fetchJobs: () => {
+    console.log("REACT_APP_API_DOMAIN:" + process.env.REACT_APP_API_DOMAIN)
+    console.log("REACT_APP_API_VERSION:" + process.env.REACT_APP_API_VERSION)
     return axios.get(process.env.REACT_APP_API_DOMAIN + process.env.REACT_APP_API_VERSION + '/dispatch.json', { crossdomain: true });
   },
   fetchAvailableTrucks: () => {
