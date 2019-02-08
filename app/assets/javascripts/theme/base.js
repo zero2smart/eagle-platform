d3b26c289d0d8c8e0c20794e808505218d39f9ce
Scott Shervington <scott@bigbadmachine.com>
2019-02-08 12:09:04 -0500

    Fixed asset paths

--- app/assets/javascripts/theme/base.js
+++ app/assets/javascripts/theme/base.js
@@ -1,44 +1,41 @@
 //** CORE
-//= require jquery/dist/jquery.min.js
-//= require popper.js/dist/umd/popper.js
-//= require bootstrap/dist/js/bootstrap.min.js
+// require jquery/dist/jquery.min.js
+// require jquery-ui.min.js
+// require popper.js/dist/umd/popper.js
+// require bootstrap/dist/js/bootstrap.min.js
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
 
-//= require simplebar/dist/simplebar.min.js
+//= require simplebar.min.js
 
-//= require dom-factory/dist/dom-factory.js
-//= require material-design-kit/dist/material-design-kit.js
+//= require dom-factory.js
+//= require material-design-kit.js
 
 //** PLUGIN SCRIPTS (NODE_MODULES)
-//= require jquery-mask-plugin/dist/jquery.mask.min.js
-//= require select2/dist/js/select2.min.js
+//= require jquery.mask.min.js
+//= require select2.min.js
 
-//= require moment/min/moment.min.js
-//= require moment-range/dist/moment-range.js
+//= require moment.min.js
+//= require moment-range.js
 
-//= require chart.js/dist/Chart.min.js
-//= require flatpickr/dist/flatpickr.min.js
+//= require chart.min.js
+//= require flatpickr.min.js
 
-//= require jqvmap/dist/jquery.vmap.min.js
-//= require jqvmap/dist/maps/jquery.vmap.world.js
+//= require jquery.vmap.min.js
+//= require jquery.vmap.world.js
 
 //= require ./vector-maps/mall-map.js
 //= require ./vector-maps/gdp-data.js
 
-
-//= require dropzone/dist/dropzone.js
-
-//= require list.js/dist/list.min.js
-//= require toastr/toastr.js
-
-//= require dragula/dist/dragula.min.js
-
-//= require jquery-ui-dist/jquery-ui.min.js
-//= require fullcalendar/dist/fullcalendar.min.js
-
-//= require quill/dist/quill.min.js
-
-
-//=require daterangepicker/daterangepicker.js
+//= require dropzone.js
+//= require list.min.js
+//= require toastr.js
+//= require dragula.min.js
+//= require fullcalendar.min.js
+//= require quill.min.js
+//=require daterangepicker.js
 
 
 //** APP SETTINGS
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
+  before_action :redirect_to_dashboard, only: :home
 
   def home
   end
   
+  private
+  
+    def redirect_to_dashboard 
+      if current_user 
+        redirect_to dashboard_path  
+      end 
+    end
 end
