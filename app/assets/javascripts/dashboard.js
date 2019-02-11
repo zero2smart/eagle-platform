467bcc69f332da8c5ecd0d18c8283535e14f98ff
Jason Calleiro <jason@astroid.me>
2019-02-11 00:22:46 -0600

    add inline editor and material organization

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -11,3 +11,40 @@
 // about supported directives.
 //
 //= require theme/base
+
+
+$(document).ready(function() {
+  $('#matPrice').editable({
+    mode: 'inline',
+    type: 'text',
+    pk: 1,
+    url: '/post',
+    title: 'Enter username'
+  });
+
+  $('#matStatus').editable({
+    mode: 'inline',
+    type: 'select',
+    pk: 1,
+    url: '/post',
+    value: 1,
+    title: 'Enter username',
+    source: [
+      {value: 1, text: 'Active'},
+      {value: 2, text: 'Inactive'},
+    ]
+  });
+
+  $('#matUnit').editable({
+    mode: 'inline',
+    type: 'select',
+    pk: 1,
+    url: '/post',
+    value: 1,
+    title: 'Enter username',
+    source: [
+      {value: 1, text: 'Yard'},
+      {value: 2, text: 'Ton'},
+    ]
+  });
+});
