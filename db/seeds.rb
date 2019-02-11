7432bda00cba220d8a519e014f3a30eedfc8d400
Scott Shervington <scott@bigbadmachine.com>
2019-02-11 02:01:06 -0500

    Added destroys to rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+if Rails.env.development?
+  Vendor.destroy_all
+  User.destroy_all
+  Truck.destroy_all
+end
 
 haul_admin = User.find_or_initialize_by(email: "admin@haulhq.com")
 haul_admin.name = "Haul Admin"
