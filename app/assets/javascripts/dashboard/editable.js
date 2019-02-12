a3b1dffd22389da461ce9504fe0df577d4c7becd
Jason Calleiro <jason@astroid.me>
2019-02-11 22:18:22 -0600

    update pricing

--- app/assets/javascripts/dashboard/editable.js
+++ app/assets/javascripts/dashboard/editable.js
@@ -1,42 +1,12 @@
 $(document).ready(function() {
   
-  if( $('#matPrice').length ){
-    $('#matPrice').editable({
+  if( $('#matPrice1, #matPrice2, #matPrice3').length ){
+    $('#matPrice1, #matPrice2, #matPrice3').editable({
       mode: 'inline',
       type: 'text',
       pk: 1,
       url: '/post',
-      title: 'Enter username'
-    });
-  }
-
-  if( $('#matStatus').length ){
-    $('#matStatus').editable({
-      mode: 'inline',
-      type: 'select',
-      pk: 1,
-      url: '/post',
-      value: 1,
-      title: 'Enter username',
-      source: [
-        {value: 1, text: 'Active'},
-        {value: 2, text: 'Inactive'},
-      ]
-    });
-  }
-
-  if( $('#matUnit').length ){
-    $('#matUnit').editable({
-      mode: 'inline',
-      type: 'select',
-      pk: 1,
-      url: '/post',
-      value: 1,
-      title: 'Enter username',
-      source: [
-        {value: 1, text: 'Yard'},
-        {value: 2, text: 'Ton'},
-      ]
+      title: 'Enter Price'
     });
   }
   
